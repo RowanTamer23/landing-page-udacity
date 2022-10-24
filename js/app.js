769fe7e3eed8e76e01a8803bfdd9d2b1b3bb4ba2
RowanTamer23 <rowan.tamer232001@gmail.com>
2022-10-24 16:26:43 +0200

    Final

--- js/app.js
+++ js/app.js
@@ -1,75 +1,95 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
-*/
+ *
+ */
+
+let list = document.querySelector("#navbar__list");
+let sections = document.querySelectorAll("section");
+
+let options = {
+  root: null, // relative to document viewport
+  rootMargin: "10px", // margin around root. Values are similar to css property. Unitless values not allowed
+  threshold: 0.7, // visible amount of item shown in relation to root
+};
+
+let observer = new IntersectionObserver((sections) => {
+  sections.forEach((s) => {
+    const link = list.querySelector(
+      `[data-nav='${s.target.getAttribute("data-nav")}']`
+    );
+
+    if (s.isIntersecting) {
+      s.target.classList.add("your-active-class");
+      link.classList.add("active");
+    } else {
+      s.target.classList.remove("your-active-class");
+      link.classList.remove("active");
+    }
+  });
+}, options);
+
 const navUpdate = () => {
-let list = document.querySelector('#navbar__list');
-let sections = document.querySelectorAll('section');
-sections.forEach(s=>{
-    const navLink = document.createElement('li');
-    const sectionTitle = s.getAttribute('data-nav')
-    const SectionId = s. getAttribute('id');
-    navLink.classList.add('menu__link');
-    navLink.innerHTML=`<a style='color:black; text-decoration:none;' href=${'#'+SectionId}>${sectionTitle}</a>`
-    list.appendChild(navLink)
-})
-}
+  sections.forEach((s) => {
+    const navLink = document.createElement("li");
+    const sectionTitle = s.getAttribute("data-nav");
+    const SectionId = s.getAttribute("id");
+    navLink.classList.add("menu__link");
+    navLink.setAttribute("data-nav", sectionTitle);
+    navLink.innerHTML = `<a style='color:inherit; text-decoration:none;' href=${
+      "#" + SectionId
+    }>${sectionTitle}</a>`;
+    list.appendChild(navLink);
+    observer.observe(s);
+  });
+};
 navUpdate();
 
 /**
  * Comments should be present at the beginning of each procedure and class.
  * Great to have comments before crucial code sections within the procedure.
-*/
+ */
 
 /**
  * Define Global Variables
- * 
-*/
-
+ *
+ */
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
-*/
-
-
+ *
+ */
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
-*/
+ *
+ */
 
 // build the nav
 
-
 // Add class 'active' to section when near top of viewport
 
-
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
  * Begin Events
- * 
-*/
+ *
+ */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
